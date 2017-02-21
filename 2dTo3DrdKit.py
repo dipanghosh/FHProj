c7ddf011aaa8b667b12e61f02c8a35ef745a37a0
Dipan Ghosh <dipandeoghar@gmail.com>
2017-02-21 10:18:20 +0100

    .

--- 2dTo3DrdKit.py
+++ 2dTo3DrdKit.py
@@ -1,46 +1,48 @@
 '''
 Created on 30 Jan 2017
-
 @author: dghosh
-extractSavePdb
-
-
-
-
-
-
+20 Feb 2017:: Added Multithreading
 '''
+
 outputdir = '/Users/dghosh/Desktop/FreqHitterProject/Luciferase/Analysis/Docking/inactives/output/'
 import os
 import rdkit
 from rdkit import Chem
 from rdkit.Chem import AllChem
 import threading
-def extractSavePdb(outputdir, mol):    
-        molename = mol.GetProp("_Name")
-        #badFile = outputdir + molename + '.pdb'
-        #goodFile = outputdir + '/3D_minBabel/' + molename + '.pdb'
-        print molename
-        AllChem.EmbedMolecule(mol)
-        mol = Chem.AddHs(mol)
-        AllChem.EmbedMolecule(mol)
-        AllChem.UFFOptimizeMolecule(mol)
-        w = Chem.PDBWriter(outputdir + molename + '.pdb')
-        w.write(mol)
-    
+from multiprocessing import Pool
+
+def extractSavePdb(outputdir, mol):
+    '''
+    Takes one molecule at a time from a given moleculeSet, tries to optimise and writes it off as a PDB file
+    '''
+    molename = mol.GetProp("_Name")
+    print molename
+    AllChem.EmbedMolecule(mol)
+    mol = Chem.AddHs(mol)
+    AllChem.EmbedMolecule(mol)
+    AllChem.UFFOptimizeMolecule(mol)
+    w = Chem.PDBWriter(outputdir + molename + '.pdb')
+    w.write(mol)
+def minBabel(filename):
+    '''
+    Minimizes one molecule at a time through OpenBabel
+    '''
+    print(os.getcwd())
+    os.system('/usr/local/bin/obminimize -ff -ff Ghemical -cg -n 2500 -c 1.0E-5 -cut -rvdw6.0 -rele 10.0 -pf 10 ./output/' + filename + ' > ./3D_minBabel/' + filename)
+
 os.mkdir(outputdir)
 os.chdir(outputdir)
 os.chdir("../")
 os.mkdir('3D_minBabel')
 moleculeSet = Chem.SDMolSupplier("/Users/dghosh/Downloads/2276416762553814740.sdf")
-#print("There are %d CPUs on this machine" % mp.cpu_count())
 
+#Iterarate over moleculeSet, Generate one thread per molecule. Multithreading in order to process loads quickly...
 for mol in moleculeSet:
     t = threading.Thread(target=extractSavePdb, args = (outputdir, mol) )
     t.start()
     
-    #os.system('/usr/local/bin/obminimize -ff -ff Ghemical -cg -n 2500 -c 1.0E-5 -cut -rvdw6.0 -rele 10.0 -pf 10 '+badFile+' > '+goodFile)
-for filename in os.listdir(outputdir):
-    #print filename[-4:]
-    if filename[-4:]=='.pdb':
-        os.system('/usr/local/bin/obminimize -ff -ff Ghemical -cg -n 2500 -c 1.0E-5 -cut -rvdw6.0 -rele 10.0 -pf 10 '+filename+' > ../3D_minBabel/'+filename)
+#Generating one thread does not work for Shell, must be run as different processess. Limit the processess to 8(max # of core), employ pool of subworkers to process faster.
+pool = Pool(processes=8)
+os.chdir(outputdir)
+pool.map(minBabel, os.listdir(outputdir))
--- minbabel.py
+++ minbabel.py
@@ -4,9 +4,21 @@ Created on 20 Feb 2017
 @author: dghosh
 '''
 import os
+#import threading
+from subprocess import Popen
+#from itertools import islice
+from multiprocessing import Pool
+
 outputdir = '/Users/dghosh/Desktop/FreqHitterProject/Luciferase/Analysis/Docking/inactives/output/'
 os.chdir(outputdir)
-for filename in os.listdir(outputdir):
+def minBabel(filename):
+    os.system('/usr/local/bin/obminimize -ff -ff Ghemical -cg -n 2500 -c 1.0E-5 -cut -rvdw6.0 -rele 10.0 -pf 10 ' + filename + ' > ../3D_minBabel/' + filename)
     #print filename[-4:]
-    if filename[-4:]=='.pdb':
-        os.system('/usr/local/bin/obminimize -ff -ff Ghemical -cg -n 2500 -c 1.0E-5 -cut -rvdw6.0 -rele 10.0 -pf 10 '+filename+' > ../3D_minBabel/'+filename)
+#     if filename[-4:]=='.pdb':
+#         th = threading.Thread(target=minBabel, args = (filename,) )
+#         th.start()
+pool = Pool(processes=8)
+pool.map(minBabel, os.listdir(outputdir))
+#for command in processes:
+    #myprocessess = (Popen(command, shell=True))
+    
