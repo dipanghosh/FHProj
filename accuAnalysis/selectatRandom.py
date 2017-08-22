b762adc5d2d7acdeb3e5497789f40621438a52c9
Dipan Ghosh <dipandeoghar@gmail.com>
2017-08-22 15:21:52 +0200

    Changed file for selectatrandom

--- accuAnalysis/selectatRandom.py
+++ accuAnalysis/selectatRandom.py
@@ -1,7 +1,8 @@
-from accuAnalysis import loadfiles as ld
+#from accuAnalysis import loadfiles as ld
 from dipan_utilities import utilities
 import random
 
-set3_50Inactives = set(ld.listAll588342) - set(ld.list588342_50)
-randlist = random.sample(list(set3_50Inactives), 25000)
+with open('/Users/dghosh/Downloads/combined_inactive.csv') as f:
+    mylist = f.read().splitlines()
+randlist = random.sample(list(mylist), 10000)
 utilities.listToFile('/Users/dghosh/Desktop/randListInactive', randlist)
