f311227da52507dce2732ed89fcaaa60de899ea3
Dipan Ghosh <dipandeoghar@gmail.com>
2017-08-22 15:16:46 +0200

    Added Utility functioc dumpToPickle

--- dipan_utilities/utilities.py
+++ dipan_utilities/utilities.py
@@ -1,24 +1,31 @@
 import csv
+import pickle
+
+desktopDir = '/Users/dghosh/Desktop/'
+
 
 def csvToDict(filename):
     reader = csv.reader(open(filename, 'r'))
     dict = {}
     for row in reader:
-       v, _, k = row
+       v, k, _ = row
        dict[k] = v
     return dict
+
 
 def listToFile(filename, listname):
     with open(filename, 'w') as f:
         for line in listname:
             f.write(line+'\n')
 
+
 def pullFromDictAndWriteToFIle(yourDict, yourSet, yourFile):
     for id in yourSet:
         try:
             yourFile.write(yourDict[id] + '\n')
         except:
-            pass
+            print "Failed to transform for " + id
+
 
 def extractList(idlist, mydict):
     mylist = []
@@ -28,3 +35,8 @@ def extractList(idlist, mydict):
         except KeyError:
             print id + " Key does not exist here!"
     return mylist
+
+
+def dumpToPickle(filename, object):
+    with open(desktopDir+filename, 'wb') as fp:
+        pickle.dump(object, fp)
